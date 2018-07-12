0d6cfd295ba8edf6c9336e2980209cb91111c04d
Athan Clark <athan.clark@gmail.com>
2018-07-11 21:28:07 -0600

    support for "view symbols" - basically the kind-level type variant declaring the underlying "subject" of the schema, and views, unified

--- src/Database/KingFisher/Class.hs
+++ src/Database/KingFisher/Class.hs
@@ -8,34 +8,42 @@
 module Database.KingFisher.Class where
 
 
+import Data.Proxy (Proxy)
+
+
+
 -- * Generics for different data views
 
-class GetStorable pred a m | a -> pred where
-  getStorable :: pred -> m (Maybe a)
+class GetStorable pred view symbol m
+  | view -> pred symbol where
+  getStorable :: pred -> m (Maybe view)
 
-class GetAllStorable pred a m | a -> pred where
-  getAllStorable :: pred -> m [a]
+class GetAllStorable pred view symbol m
+  | view -> pred symbol where
+  getAllStorable :: pred -> m [view]
 
-class NewStorable key a m | a -> key where
-  newStorable :: a -> m (Maybe key)
+class NewStorable key view symbol m
+  | view -> key symbol where
+  newStorable :: view -> m (Maybe key)
 
-class SetStorable pred err a m | a -> pred err where
-  setStorable :: pred -> a -> m (Maybe err)
+class SetStorable pred err view symbol m
+  | view -> pred err symbol where
+  setStorable :: pred -> view -> m (Maybe err)
 
-class DeleteStorable pred err m | pred -> err where
-  deleteStorable :: pred -> m (Maybe err)
+class DeleteStorable pred err symbol m where
+  deleteStorable :: Proxy symbol -> pred -> m (Maybe err)
 
 
 -- | A single data view 'a' that supports CRUD
-type CRUDStorable getPred getAllPred key setPred setErr deletePred deleteErr a m =
-  ( GetStorable getPred a m
-  , GetAllStorable getAllPred a m
-  , NewStorable key a m
-  , SetStorable setPred setErr a m
-  , DeleteStorable deletePred deleteErr m
+type CRUDStorable getPred getAllPred key setPred setErr deletePred deleteErr view symbol m =
+  ( GetStorable    getPred         view symbol m
+  , GetAllStorable getAllPred      view symbol m
+  , NewStorable    key             view symbol m
+  , SetStorable    setPred setErr  view symbol m
+  , DeleteStorable deletePred deleteErr symbol m
   )
 
 -- | A single data view 'a' that supports CRUD, all sharing the same key as the
 -- predicate.
-type BasicCRUDStorable key setErr deleteErr a m =
-  CRUDStorable key () key key setErr key deleteErr a m
+type BasicCRUDStorable key setErr deleteErr view symbol m =
+  CRUDStorable key () key key setErr key deleteErr view symbol m
